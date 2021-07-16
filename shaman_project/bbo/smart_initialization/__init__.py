1e3e840f472422fc7dd2643f1ce47c04b956ad67
Gautier Valentin <valentin.gautier@atos.net>
2021-07-16 09:24:26 +0200

    Adding linux model

--- shaman_project/bbo/smart_initialization/__init__.py
+++ shaman_project/bbo/smart_initialization/__init__.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import dill
 
 CURRENT_DIR = Path(__file__).parent.resolve()
-MODEL_PATH = (CURRENT_DIR / "models" / "model.dill")
+MODEL_PATH = (CURRENT_DIR / "models" / "model_linux.dill")
 
 with open(MODEL_PATH, "rb") as f:
     model = dill.load(f)
